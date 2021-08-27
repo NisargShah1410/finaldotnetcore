88f1fed0e011e6ccb79203f7cd698f22d992b3d1
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-08-27 15:01:32 +0530

    Create azurewebapp.tf

---
+++ terraform/azurewebapp.tf
@@ -0,0 +1,29 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind = "Linux"
+  reserved = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service-tf"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    linux_fx_version = "DOTNETCORE|3.1"
+    dotnet_framework_version = "v4.0"
+  }
+}
